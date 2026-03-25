227991028699a7f320a023bbbb0e9a51bdbeb128
JoseALopezP <jalp42@live.com>
2026-03-25 18:15:54 -0300

    Fix: Node types and path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url' // Añade esto
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// Sustituimos __dirname que ya no se usa en módulos modernos
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  // SUSTITUYE 'nombre-de-tu-repositorio' por el nombre exacto que tiene en GitHub
   base: '/superCampo/', 
-  
   plugins: [
     react(),
     tailwindcss(),
